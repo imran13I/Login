4b7e3c913042e8cbfd457c1ac0c5a68e67ee9540
imran13I <imranmallouk70@gmail.com>
2025-05-10 18:36:25 +0100

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ function web() {
 	const email = document.getElementById("email").value;
 	const password = document.getElementById("password").value;
 	if (email.includes("@") && password.length >= 8) {
-		window.open("index.html",  "_blank");
+		window.open("https://web-co.netlify.app/",  "_blank");
 	}
 	else {
 		alert ("please enter your email and password");
